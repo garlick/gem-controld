1a48bd2222b5ed91d3cbce9d01e12e384603420e
Jim Garlick <garlick.jim@gmail.com>
2015-02-22 06:19:21 +0000

    gpio: assign buttons to motion controls
    N, S, E, W set configurable slew velocities (fast/slow setting).
    M1+M2 exits the program, termination all motion.
    M1 displays position (X,Y)

--- src/gem.c
+++ src/gem.c
@@ -21,7 +21,11 @@ char *prog = "";
 typedef struct {
     char *ra_device;
     int ra_track;
+    int ra_slow;
+    int ra_fast;
     char *dec_device;
+    int dec_slow;
+    int dec_fast;
     bool debug;
 } opt_t;
 
@@ -53,6 +57,10 @@ int main (int argc, char *argv[])
     opt_t opt;
 
     memset (&opt, 0, sizeof (opt));
+    opt.ra_slow = 200;
+    opt.ra_fast = 8000;
+    opt.dec_slow = 200;
+    opt.dec_fast = 8000;
     prog = basename (argv[0]);
 
     while ((ch = getopt_long (argc, argv, OPTIONS, longopts, NULL)) != -1) {
@@ -90,11 +98,6 @@ int main (int argc, char *argv[])
     if (!opt.dec_device)
         opt.dec_device = xstrdup ("/dev/ttyS1");
 
-    msg ("ra_device: %s", opt.ra_device);
-    msg ("ra_track: %d", opt.ra_track);
-    msg ("dec_device: %s", opt.dec_device);
-    msg ("debug: %s", opt.debug ? "yes" : "no");
-
     gpio_init ();
 
     /* Initialize RA
@@ -108,18 +111,63 @@ int main (int argc, char *argv[])
             err_exit ("ra set velocity: %s", opt.ra_device);
     }
 
+    /* Initialize DEC
+     */
     motion_t dec;
     dec = motion_init (opt.dec_device, "DEC", opt.debug ? MOTION_DEBUG : 0);
 
-    int keys;
-    for (;;) {
-        keys = gpio_event ();
-        msg ("gpio: 0x%x", keys);
+    /* Respond to button presses.
+     */
+    bool bye = false;
+    while (!bye) {
+        int keys = gpio_event ();
+        bool fast = (keys & 0x8);
+        switch (keys & 0x7) {
+            case 0: /* nothing */
+                if (motion_set_velocity (ra, opt.ra_track) < 0)
+                    err_exit ("ra set velocity");
+                if (motion_set_velocity (dec, 0) < 0)
+                    err_exit ("dec set velocity");
+                break;
+            case 1: /* N */
+                if (motion_set_velocity (dec, fast ? opt.dec_fast
+                                                   : opt.dec_slow) < 0)
+                    err_exit ("ra set velocity");
+                break;
+            case 2: /* S */
+                if (motion_set_velocity (dec, fast ? -1*opt.dec_fast
+                                                   : -1*opt.dec_slow) < 0)
+                    err_exit ("ra set velocity");
+                break;
+            case 3: /* W */
+                if (motion_set_velocity (ra, fast ? opt.ra_fast
+                                                  : opt.ra_slow) < 0)
+                    err_exit ("ra set velocity");
+                break;
+            case 4: /* E */
+                if (motion_set_velocity (ra, fast ? -1*opt.ra_fast
+                                                  : -1*opt.ra_slow) < 0)
+                    err_exit ("ra set velocity");
+                break;
+            case 5: { /* M1 */
+                double x, y;
+                if (motion_get_position (ra, &x) < 0
+                 || motion_get_position (dec, &y) < 0)
+                    err_exit ("failed to get x,y position");
+                msg ("ra=%.1f dec=%.1f", x, y);
+                break;
+            }
+            case 6: /* M2 */
+                break;
+            case 7: /* M1+M2 */
+                bye = true;
+                break;
+        }
     }
 
     gpio_fini ();
 
-    //motion_fini (ra); 
+    motion_fini (ra); 
 
     return 0;
 }
@@ -143,12 +191,20 @@ int config_cb (void *user, const char *section, const char *name,
             opt->ra_device = xstrdup (value);
         } else if (!strcmp (name, "track")) {
             opt->ra_track = strtoul (value, NULL, 10);
+        } else if (!strcmp (name, "slow")) {
+            opt->ra_slow = strtoul (value, NULL, 10);
+        } else if (!strcmp (name, "fast")) {
+            opt->ra_fast = strtoul (value, NULL, 10);
         }
     } else if (!strcmp (section, "dec")) {
         if (!strcmp (name, "device")) {
             if (opt->dec_device)
                 free (opt->dec_device);
             opt->dec_device = xstrdup (value);
+        } else if (!strcmp (name, "slow")) {
+            opt->dec_slow = strtoul (value, NULL, 10);
+        } else if (!strcmp (name, "fast")) {
+            opt->dec_fast = strtoul (value, NULL, 10);
         }
     }
     return 0;
